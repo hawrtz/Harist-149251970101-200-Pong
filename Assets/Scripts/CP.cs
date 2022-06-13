f247cec29193934f89fc2685f42bf9118ea760d9
hawrtz <61041915+hawrtz@users.noreply.github.com>
2022-06-13 07:51:55 +0700

    Base Gameplay

---
+++ Assets/Scripts/CP.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CP : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private Rigidbody2D rig;
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void Update()
+    {
+        //Gerakan Objek 
+        MoveObject(GetInput());
+    }
+
+    private Vector2 GetInput()
+    {
+        Vector2 movement = Vector2.zero;
+
+        if (Input.GetKey(upKey))
+        {
+            return Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey))
+        {
+            return Vector2.down * speed;
+        }
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement)
+    {
+        rig.velocity = movement;
+    }
+
+
+
+}
